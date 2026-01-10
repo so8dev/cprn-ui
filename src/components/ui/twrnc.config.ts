606beff10fea5aa3fae307ec0f76eb8fa6c176e4
so8dev <73172370+so8dev@users.noreply.github.com>
2026-01-11 00:51:28 +0900

    feat: twrnc with cva

---
+++ src/components/ui/twrnc.config.ts
@@ -0,0 +1,6 @@
+import { create } from "twrnc";
+
+// biome-ignore lint/style/noCommonJs: https://github.com/jaredh159/tailwind-react-native-classnames
+const tw = create(require(`./tailwind.config.js`));
+
+export default tw;
